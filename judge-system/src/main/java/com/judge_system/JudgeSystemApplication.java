291879eb4fda22c0b05a2a5c642a8c2929210915
wastedx7 <wastedx7@gmail.com>
2026-06-14 14:14:05 +0530

    shift to java instantiation

---
+++ judge-system/src/main/java/com/judge_system/JudgeSystemApplication.java
@@ -0,0 +1,13 @@
+package com.judge_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JudgeSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JudgeSystemApplication.class, args);
+	}
+
+}
---
+++ judge-system/src/test/java/com/judge_system/JudgeSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.judge_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JudgeSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
